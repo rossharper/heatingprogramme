56ae8118fffc04f496dbdc90d608d1aa3b6c9954
Ross Harper <ross.harper@bbc.co.uk>
2015-11-08 10:26:41 +0000

    async programme file reload

--- ProgrammeProvider.js
+++ ProgrammeProvider.js
@@ -7,7 +7,7 @@ var PROGRAMME_FILE = "programme.json";
 
 function ProgrammeProvider(programmeDataPath) {
 
-    var programme = new Programme(loadProgrammeFile());
+    var programme = new Programme(loadProgrammeFileSync());
     watchProgrammeFile();
 
     this.getProgramme = function() {
@@ -18,20 +18,42 @@ function ProgrammeProvider(programmeDataPath) {
         return programmeDataPath + "/" + PROGRAMME_FILE;
     }
 
-    function readProgrammeFile(programmeFilePath) {
+    function readProgrammeFileSync(programmeFilePath) {
         var file = fs.readFileSync(programmeFilePath, "utf8");
         return JSON.parse(file);
     }
 
-    function loadProgrammeFile() {
+    function loadProgrammeFileSync() {
         var programmeFilePath = getProgrammeDataFilePath();
         try{
-            return readProgrammeFile(programmeFilePath);
+            return readProgrammeFileSync(programmeFilePath);
         }
         catch(e) {
             if (e.code === 'ENOENT') {
                 console.log("Programme data file missing: " + programmeFilePath);
-                return readProgrammeFile(DEFAULT_PROGRAMME_FILE);
+                return readProgrammeFileSync(DEFAULT_PROGRAMME_FILE);
+            } else {
+                throw e;
+            }
+        }
+    }
+
+    function readProgrammeFile(programmeFilePath, callback) {
+        fs.readFile(programmeFilePath, function (err, data) {
+            if (err) throw err;
+            callback(JSON.parse(data));
+        });
+    }
+
+    function loadProgrammeFile(callback) {
+        var programmeFilePath = getProgrammeDataFilePath();
+        try{
+            readProgrammeFile(programmeFilePath, callback);
+        }
+        catch(e) {
+            if (e.code === 'ENOENT') {
+                console.log("Programme data file missing: " + programmeFilePath);
+                readProgrammeFile(DEFAULT_PROGRAMME_FILE, callback);
             } else {
                 throw e;
             }
@@ -40,7 +62,10 @@ function ProgrammeProvider(programmeDataPath) {
 
     function onProgrammeFileChange() {
         console.log("Programme file changed. Reloading.");
-        programme = new Programme(loadProgrammeFile());
+        //programme = new Programme(loadProgrammeFileSync());
+        loadProgrammeFile(function(programmeJson) {
+            programme = new Programme(programmeJson);
+        });
     }
 
     function watchProgrammeFile() {
