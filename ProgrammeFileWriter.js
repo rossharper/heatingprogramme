36018c894357f8b86bb98c09a3c23201b3789144
Ross Harper <rossharper@users.noreply.github.com>
2015-11-09 09:53:48 +0000

    Update ProgrammeFileWriter.js

--- ProgrammeFileWriter.js
+++ ProgrammeFileWriter.js
@@ -3,6 +3,7 @@ var ProgrammeFile = require('./ProgrammeFile');
 
 module.exports = {
     writeProgramme : function(programmeDataPath, programme, callback) {
+        // TODO: the programme data needs to go via JSON.stringify
         fs.writeFile(ProgrammeFile.getProgrammeDataFilePath(programmeDataPath), programme.getProgrammeData(), function(err) {
             if(err) {
                 return console.log(err);
