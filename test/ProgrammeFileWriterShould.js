f2ac73d7093ca52c793fef0959f9c4746202cfd9
Ross Harper <ross@rosss-mbp-16.home>
2021-02-25 17:16:10 +0000

    Add test for writing programme file

--- test/ProgrammeFileWriterShould.js
+++ test/ProgrammeFileWriterShould.js
@@ -1,10 +1,53 @@
+'use strict';
+
+const { assert } = require('chai');
+const fs = require('fs');
+const path = require('path');
 var chai = require('chai');
+const ProgrammeFileWriter = require('../ProgrammeFileWriter');
 var expect = chai.expect; // we are using the "expect" style of Chai
 var Programme = require('./../Programme');
 var ProgrammeFileLoader = require('./../ProgrammeFileLoader');
 
-describe.skip('ProgrammeFileWriter should', function() {
-    it('should do something', function(done) {
-        done();
+const PATH = '.';
+const FILE_NAME = 'programme.json';
+const FILE_PATH = path.join(PATH, '/', FILE_NAME);
+
+describe('ProgrammeFileWriter should', function() {
+
+    beforeEach(() => {
+        try {
+            deleteFile();
+        } catch (e) {
+            if (e.code !== 'ENOENT') {
+                assert.fail('error cleaning up test file');
+            }
+        }
+    });
+
+    afterEach(() => {
+        try {
+            deleteFile();
+        } catch (e) {
+            assert.fail('error cleaning up test file');
+        }
+    });
+
+    function deleteFile() {
+        fs.unlinkSync(FILE_PATH);
+    }
+
+    it('successfully write programme file', function(done) {
+        ProgrammeFileLoader.loadProgramme(PATH, function(programme) {
+            programme.setComfortSetPoint(30);
+            programme.setHeatingOff();
+            ProgrammeFileWriter.writeProgramme(PATH, programme, function() {
+                ProgrammeFileLoader.loadProgramme(PATH, function(writtenProgramme) {
+                    expect(programme.getComfortSetPoint()).to.equal(30);
+                    expect(programme.isHeatingEnabled()).to.equal(false);
+                    done();
+                });
+            });
+        });
     });
   });
