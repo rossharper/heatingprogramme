3e767faaab06f89d8ca072b95257446a3e1ca187
Ross Harper <ross@rosss-mbp-16.home>
2021-02-25 12:56:46 +0000

    Add programme file loader tests

---
+++ test/ProgrammeFileWriterShould.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+var expect = chai.expect; // we are using the "expect" style of Chai
+var Programme = require('./../Programme');
+var ProgrammeFileLoader = require('./../ProgrammeFileLoader');
+
+describe.skip('ProgrammeFileWriter should', function() {
+    it('should do something', function(done) {
+        done();
+    });
+  });
--- test/ProgrammeShould.js
+++ test/ProgrammeShould.js
@@ -3,19 +3,29 @@ var expect = chai.expect; // we are using the "expect" style of Chai
 var Programme = require('./../Programme');
 var ProgrammeFileLoader = require('./../ProgrammeFileLoader');
 
-describe('Programme', function() {
-    it('should return comfort setpoint', function(done) {
-      ProgrammeFileLoader.loadProgramme('.', function(programme) {
-        expect(programme.getComfortSetPoint()).to.equal(20);
+describe.skip('Programme', function() {
+    it('should do something', function(done) {
         done();
-      });
     });
 
-    it('should update comfort setpoint correctly', function(done) {
+    it('should return comfort periods for Saturday', function(done) {
+      // TODO: inject fixture
       ProgrammeFileLoader.loadProgramme('.', function(programme) {
-        programme.setComfortSetPoint(10);
-        expect(programme.getComfortSetPoint()).to.equal(10);
-        done();
+          var date = new Date('January 9, 2016 00:00:00'); // a saturday
+  
+          var expected = [
+              {
+                  "startTime" : "06:30",
+                  "endTime" : "10:30"
+              },
+              {
+                  "startTime" : "17:30",
+                  "endTime" : "23:30"
+              }
+          ];
+  
+          expect(programme.getComfortPeriodsForDate(date)).to.deep.have.same.members(expected);
+          done();
       });
     });
-  });
+});
