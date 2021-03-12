24801ebc387bf8126175efc2ce04c265052670fb
Ross Harper <ross@rosss-mbp-16.home>
2021-03-12 19:23:52 +0000

    Test programme change watcher

--- ProgrammeChangeWatcher.js
+++ ProgrammeChangeWatcher.js
@@ -1,8 +1,8 @@
 'use strict'
 
-const FileWatcher = require('./FileWatcher')
 const ProgrammeFileLoader = require('./ProgrammeFileLoader')
 const ProgrammeFile = require('./ProgrammeFile')
+const Chokidar = require('chokidar')
 
 function loadProgramme (programmeDataPath, callback) {
     ProgrammeFileLoader.loadProgramme(programmeDataPath, function (loadedProgramme) {
@@ -12,14 +12,37 @@ function loadProgramme (programmeDataPath, callback) {
 }
 
 function watchProgrammeFile (programmeDataPath, onProgrammeChange) {
-    FileWatcher.watchFile(ProgrammeFile.getProgrammeDataFilePath(programmeDataPath), function () {
+    const file = ProgrammeFile.getProgrammeDataFilePath(programmeDataPath)
+    const watcher = Chokidar.watch(file)
+    const log = console.log.bind(console)
+
+    watcher
+        .on('error', function (error) {
+            log('Error happened', error)
+            onProgrammeChange(null, error)
+        })
+
+    function onChange () {
         console.log('Programme file changed. Reloading.')
         loadProgramme(programmeDataPath, onProgrammeChange)
+    }
+
+    watcher.on('ready', function () {
+        log('Watching ' + file + ' for changes...')
+        watcher
+            .on('add', onChange)
+            .on('change', onChange)
     })
+
+    return {
+        stop: function () {
+            watcher.close()
+        }
+    }
 }
 
 module.exports = {
     watchForChanges: function (programmeDataPath, onProgrammeChange) {
-        watchProgrammeFile(programmeDataPath, onProgrammeChange)
+        return watchProgrammeFile(programmeDataPath, onProgrammeChange)
     }
 }
--- test/ProgrammeChangeWatcherShould.js
+++ test/ProgrammeChangeWatcherShould.js
@@ -1,12 +1,73 @@
 'use strict'
 
+const { expect } = require('chai')
 // const chai = require('chai')
 // const expect = chai.expect // we are using the "expect" style of Chai
 // const Programme = require('./../Programme')
 // const ProgrammeFileLoader = require('./../ProgrammeFileLoader')
+const fs = require('fs')
+const path = require('path')
+const programmeChangeWatcher = require('./../ProgrammeChangeWatcher')
 
-describe.skip('ProgrammeFileWatcher', function () {
-    it('should do something', function (done) {
-        done()
+const PATH = '.'
+const FILE_NAME = 'programme.json'
+const FILE_PATH = path.join(PATH, '/', FILE_NAME)
+
+describe('ProgrammeFileWatcher', function () {
+    let watcher
+
+    beforeEach(() => {
+        try {
+            deleteFile()
+        } catch (e) {}
+        writeFile({})
+    })
+
+    afterEach(() => {
+        watcher.stop()
+        try {
+            deleteFile()
+        } catch (e) {}
+    })
+
+    function writeFile (programmeData) {
+        fs.writeFile(FILE_PATH, JSON.stringify(programmeData), function (err) {
+            if (err) {
+                console.log(`Error writing programme file: ${err}`)
+                throw err
+            }
+
+            console.log('Programme data file saved.')
+        })
+    }
+
+    function deleteFile () {
+        fs.unlinkSync(FILE_PATH)
+    }
+    it('should callback with new programme when new file written', function (done) {
+        watcher = programmeChangeWatcher.watchForChanges(PATH, function (programme) {
+            expect(programme.getComfortSetPoint()).to.equal(21)
+            watcher.stop()
+            done()
+        })
+
+        writeFile({ comfortTemp: 21 })
+    })
+
+    it('should callback a second time when written again', function (done) {
+        let calls = 0
+        watcher = programmeChangeWatcher.watchForChanges(PATH, function (programme) {
+            calls++
+            if (calls < 2) {
+                expect(programme.getComfortSetPoint()).to.equal(21)
+                writeFile({ comfortTemp: 22 })
+            } else {
+                expect(programme.getComfortSetPoint()).to.equal(22)
+                watcher.stop()
+                done()
+            }
+        })
+
+        writeFile({ comfortTemp: 21 })
     })
 })
